3295de1735fbf5161e6b38e891354ff11c2e9ee0
Ben Ashman <ben@benashman.co.uk>
2012-09-19 13:39:24 +0100

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-$(function() {});
+(function($) {
+
+	$.fn.extend({
+
+		readingTime: function() {
+
+			var defaults = {
+				wpm: 200
+			}
+
+			var opts = $.extend(defaults, opts);
+
+			return this.each(function(){
+				var o = opts;
+
+				var wordCount = $(this).text().split(" ").length;
+
+				$(this).append(wordCount);
+
+			});
+
+		}
+
+	});
+
+})(jQuery);

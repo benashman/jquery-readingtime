742480f11f81ae4359abc712485052dcd41d7095
Ben Ashman <ben@benashman.co.uk>
2012-09-19 15:46:54 +0100

    Added container class and prefix options

--- js/app.js
+++ js/app.js
@@ -1,26 +1,50 @@
+/*
+ *	jquery.readingTime.js
+ * 	Display an estimated reading time for your blog posts etc.
+ * 	
+ * 	@benashman
+ *  
+ */
+
 (function($) {
 
-	$.fn.extend({
+	$.fn.readingTime = function() {
 
-		readingTime: function() {
-
-			var defaults = {
-				wpm: 200
-			}
-
-			var opts = $.extend(defaults, opts);
-
-			return this.each(function(){
-				var o = opts;
-
-				var wordCount = $(this).text().split(" ").length;
-
-				$(this).append(wordCount);
-
-			});
-
+		//Set deafults
+		var defaults = {
+			wpm: 200,
+			prefix: "Reading Time: ",
+			containerClass: "reading-time"
 		}
 
-	});
+		//Extend default options with those provided
+		var options = $.extend(defaults, options);
+
+		//Apply to each article
+		return this.each(function() {
+
+			var $this = $(this);
+			var o = options;
+
+			//Get article word-count
+			var wordCount = $this.text().split(" ").length;
+
+			//Calculate estimated reading time based on default/user WPM rate
+			var minutes = Math.floor(wordCount / o.wpm);
+			var seconds = Math.floor(wordCount % o.wpm / (o.wpm / 60));
+
+			//Prettify estimate
+			var estimate = 	o.prefix;
+				estimate += (minutes <= 9 ? "0" : "");
+				estimate += minutes; 
+				estimate += ":"; 
+				estimate += (seconds <= 9 ? "0" : "");
+				estimate += seconds;
+
+			//Display estimated reading time for article
+			$this.append('<div class="' + o.containerClass + '"><span>' + estimate + '</span></div>');
+		});
+
+	};
 
 })(jQuery);
